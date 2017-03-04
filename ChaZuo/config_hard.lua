9f048ea6c8fc8728e1b02e2e57e9d0bf2262532c
nodemcu-dht-sonoff <33622733@qq.com>
2017-03-04 21:57:09 +0800

    hardware setting fo Sonoff S20

---
+++ ChaZuo/config_hard.lua
@@ -0,0 +1,13 @@
+-- GPIOS
+GPIO_LED = 8  --sonoff is 7
+GPIO_BL_LED = 7  --the second led 
+GPIO_SWITCH = 6  --sonoff is 6
+GPIO_DHT = 5 --sonoff is 5
+GPIO_BUTTON = 3  --sonoff is 3
+-- Alarms
+WIFI_ALARM_ID = 0
+WIFI_LED_BLINK_ALARM_ID = 1
+BUTTON_ID = 2
+WIFISETUP_ID = 3
+-- Confirmation message
+print("\nGlobal variables loaded...\n")
