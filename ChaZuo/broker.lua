34a7267c913a64bebdaa92f1d21a15b2bb1f2e96
nodemcu-dht-sonoff <33622733@qq.com>
2017-03-04 21:51:56 +0800

    Create broker.lua

---
+++ ChaZuo/broker.lua
@@ -0,0 +1,99 @@
+local dispatcher = {}
+
+function sendData()
+		gpio.write(GPIO_BL_LED,gpio.LOW)
+	local temp, humi,stat = getTempHumi()
+		if (stat == dht.OK ) then
+			tempjson="{\"temperature\":\""..temp.."\",\"humidity\":\""..humi.."\"}"
+			print("send .." .. tempjson)
+			m:publish(MQTT_MAINTOPIC .."/sensor",tempjson,0,0)
+		end
+		--if (gpio.read(GPIO_LED) == 0) then 
+		--		m:publish(MQTT_MAINTOPIC .."/stat","ON",0,0)
+		--		print("send .." .. "ON")
+		--else
+		--		m:publish(MQTT_MAINTOPIC .."/stat","OFF",0,0)
+		--		print("send .." .. "OFF")
+		--end
+		--blinking({200,2000}) 
+		gpio.write(GPIO_BL_LED,gpio.HIGH)
+end
+
+function getTempHumi()
+  local status,temp,humi,temp_decimial,humi_decimial = dht.read(GPIO_DHT)
+  if( status == dht.OK ) then
+    -- Float firmware using this example
+    --print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+  elseif( status == dht.ERROR_CHECKSUM ) then
+    print( "DHT Checksum error." );
+  elseif( status == dht.ERROR_TIMEOUT ) then
+    print( "DHT Time out." );
+  end
+  return temp, humi,status
+end
+
+
+-- client activation
+m = mqtt.Client(MQTT_CLIENTID, 60, MQTT_USERNAME,MQTT_PASSWORD) -- no pass !
+
+-- actions
+function switch_power(m, pl)
+	if pl == "on" then
+		gpio.write(GPIO_SWITCH, gpio.HIGH)
+		gpio.write(GPIO_LED,gpio.LOW)
+		m:publish(MQTT_MAINTOPIC.."/stat","ON",0,0)
+		print("MQTT : plug ON for ", MQTT_CLIENTID)
+	elseif( pl == "off") then 
+		gpio.write(GPIO_SWITCH, gpio.LOW)
+		gpio.write(GPIO_LED,gpio.HIGH)
+		m:publish(MQTT_MAINTOPIC.."/stat","OFF",0,0)
+		print("MQTT : plug OFF for ", MQTT_CLIENTID)
+	elseif( pl == "?" ) then  --返回状态
+		if(gpio.read(GPIO_SWITCH) == 0) then
+			m:publish(MQTT_MAINTOPIC.."/stat","ON",0,0)
+			else
+			m:publish(MQTT_MAINTOPIC.."/stat","OFF",0,0)
+		end
+	end
+end
+
+
+-- events
+m:lwt('/lwt', MQTT_CLIENTID .. " died !", 0, 0)
+
+m:on('connect', function(m)
+	print('MQTT : ' .. MQTT_CLIENTID .. " connected to : " .. MQTT_HOST .. " on port : " .. MQTT_PORT)
+	MQTTReady = 1
+	m:subscribe(MQTT_MAINTOPIC..'/', 0, function (m)
+		print('MQTT : subscribed to ', MQTT_MAINTOPIC) 
+		--if GPIO_DHT~=nil then 
+			tmr.alarm(3,5000,1,sendData)
+		--end
+	end)
+	gpio.trig(GPIO_BUTTON, "up", M_button_press )
+end)
+
+m:on('offline', function(m)
+	MQTTReady = 0
+    ip = wifi.sta.getip()
+    print ("MQTT reconnecting to " .. mqttBroker .. " from " .. ip)
+    tmr.alarm(3, 10000, 0, function()
+        --node.restart();
+		--blinking({1000,2000}) 
+    end)
+end)
+
+m:on('message', function(m, topic, pl)
+	print('MQTT : Topic ', topic, ' with payload ', pl)
+		switch_power(m,pl)
+
+end)
+
+
+
+-- Start
+gpio.mode(GPIO_SWITCH, gpio.OUTPUT)
+gpio.mode(GPIO_LED, gpio.OUTPUT)
+gpio.mode(GPIO_BL_LED, gpio.OUTPUT)
+dispatcher[MQTT_MAINTOPIC] = switch_power
+m:connect(MQTT_HOST, MQTT_PORT, 0, 1)
